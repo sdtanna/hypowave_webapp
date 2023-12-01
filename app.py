130880e115ec2a91f41e7deea8ebbd9b7096ecfc
sdtanna <tannas4@mcmaster.ca>
2023-12-01 17:08:07 -0500

    auto refesh

--- app.py
+++ app.py
@@ -4,15 +4,13 @@ import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your secret key
-socketio = SocketIO(app,async_mode='gevent')
+socketio = SocketIO(app)
 
-# Global variable to store the latest packet
 latest_packet = None
 
 @app.route('/')
 def index():
-    # Render the index template with the latest packet
-    return render_template('index.html', packet=latest_packet)
+    return render_template('index.html')
 
 @socketio.on('connect')
 def handle_connect():
@@ -27,7 +25,7 @@ def handle_my_custom_event(json_data):
     global latest_packet
     print('received json:', json_data)
     latest_packet = json_data  # Update the latest packet
-    emit('response_event', {'data': 'Response from server'})
+    emit('update_packet', latest_packet, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5001, debug=True)
